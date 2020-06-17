4ce8532b3c976e0c1cfce0eccd9976653715965d
Ravichandran <passion1>
2020-06-17 22:33:07 +0530

    Cleanup

--- src/Product.js
+++ src/Product.js
@@ -1,58 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Product = (props) => {
-    return (
-    
-        <section className="container my-5">
+function mapStateToProps(state) {
+   return {
+      categoryList: state.categoryList,
+   };
+}
+
+class Product extends Component {
+
+   constructor(props) {
+      super(props);
+      this.editClickHandler = this.editClickHandler.bind(this);
+      this.saveClickHandler = this.saveClickHandler.bind(this);
+
+      this.state = {editable: false, price:1000 }
+   }
+
+   componentDidMount() {
+      this.setState({price: this.props.vo.price})
+   }
+
+   editClickHandler() {      
+      this.setState({editable: !this.state.editable})
+   }
+
+   saveClickHandler() {
+//       this.setState({editable: !this.state.editable})
+   }
+   
+   render() {
+      
+      return (
+         <section className="container my-5">
             <div className="row row-cols-1 row-cols-md-2">
                <div className="col mb-3">
                   <article className="card border-0">
                      <figure>
-                        <img className="card-img-top mb-0" src={(props.vo.imageUrl === "/no_menu_image.jpg") ? "../assets/img/default.jpg" : props.vo.imageUrl } alt="placeholder" />
+                        <img className="card-img-top mb-0" src={(this.props.vo.imageUrl === "/no_menu_image.jpg") ? "../assets/img/default.jpg" : this.props.vo.imageUrl } alt="placeholder" />
                         <div className="card-img-overlay p-2">
-                        {props.vo.available === true ? <button className="btn btn-success btn-sm rounded-pill py-0">Available</button> : <button className="btn btn-danger btn-sm rounded-pill py-0">Not Available</button>}
+                        {this.props.vo.available === true ? <button className="btn btn-success btn-sm rounded-pill py-0">Available</button> : <button className="btn btn-danger btn-sm rounded-pill py-0">Not Available</button>}
     
                         </div>
                      </figure>
                   </article>
                </div>
                <div className="col mb-3 pt-5">
-                  <h1>{props.vo.name}</h1>
-                  <p className="lead">{ (props.vo.description === "") ? "No description found for  " + props.vo.name + ", will be updated soon" : props.vo.description} </p>
-                  <h1 className="d-block h6 mb-0">{props.vo.category}</h1>
+                  <h1>{this.props.vo.name}</h1>
+                  <p className="lead">{ (this.props.vo.description === "") ? "No description found for  " + this.props.vo.name + ", will be updated soon" : this.props.vo.description} </p>
+                  <h1 className="d-block h6 mb-0">{this.props.vo.category}</h1>
                   
                   <div className="d-flex justify-content-start align-items-center py-4 g-font-size-14">
-                     <span className="g-font-size-22"><strong>${props.vo.price}</strong> </span>
-                     <span className="px-2">${props.vo.tax}</span>
-                     <span className="text-danger">($ {props.vo.price + props.vo.tax })</span>
+                     <span className="g-font-size-22"><strong>${this.state.price}</strong> </span>
+                     <span className="px-2">${this.props.vo.tax}</span>
+                     <span className="text-danger">($ {this.props.vo.price + this.props.vo.tax })</span>
                   </div>
+                  <form id="form1" className={this.state.editable  ? '' : 'd-none' } >
                   <div className="form-group form-inline">
-                     <label for="exampleInputPassword1">Updated New Price</label>
-                     <input type="Number" class="form-control ml-3" id="exampleInputPassword1" defaultValue={props.vo.price} />
-                     
+                     <label>Updated New Price</label>
+                     <input type="Number" className="form-control ml-3" defaultValue={this.props.vo.price} />                     
                   </div>
                   <div className="form-group form-inline">
                      <span>Is Available ?</span>
-                     <input type="Radio" class="form-control ml-2" name="radio1" id="radio-available" checked />
-                     <label className="ml-2" for="exampleInputPassword1">Yes</label>
+                     <input type="Radio" className="form-control ml-2" name="radio1" defaultChecked  />
+                     <label className="ml-2">Yes</label>
 
-                     <input type="Radio" class="form-control ml-2" name="radio1" id="radio-not-available"  />
-                     <label className="ml-2" for="exampleInputPassword1">No</label>
-
-                     
+                     <input type="Radio" className="form-control ml-2" name="radio1" />
+                     <label className="ml-2" >No</label>                     
                   </div>
+                  </form>
+
                   <div className="d-flex">
                      <button className="btn btn-primary rounded-pill px-4"><i className="fa fa-spinner fa-spin mr-2" aria-hidden="true" /> Buy Now </button>
-                     <button className="btn btn-warning rounded-pill px-4 ml-2"><i className="fa fa-pencil mr-2" aria-hidden="true" /> Edit
+                     <button onClick={this.editClickHandler} className="btn btn-warning rounded-pill px-4 ml-2"><i className="fa fa-pencil mr-2" aria-hidden="true" /> Edit
                      </button>
-                     <button className="btn btn-success rounded-pill px-4 ml-2"><i className="fa fa-save mr-2" aria-hidden="true" /> Save
+                     <button onClick={this.saveClickHandler} className="btn btn-success rounded-pill px-4 ml-2"><i className="fa fa-save mr-2" aria-hidden="true" /> Save
                      </button>
                   </div>
                </div>
             </div>
          </section>
-        
-    );
-};
+      );
+   }
+}
 
-export default Product;
+export default connect(mapStateToProps)(Product);
